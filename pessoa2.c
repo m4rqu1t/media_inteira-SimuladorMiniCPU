a52985b6429d72f5adfd3d35d0067c25bfc79632
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2026-03-19 09:47:00 -0300

    Parte do código pessoa 2 - Gustavo Castro

---
+++ pessoa2.c
@@ -0,0 +1,10 @@
+void decode_execute(uint8_t op, uint8_t a, uint8_t b) {
+    switch (op) {
+        case 0x01: reg[a] = mem[b]; break;
+        case 0x02: mem[b] = reg[a]; break;
+        case 0x03: reg[a] = reg[a] + reg[b]; break;
+        case 0x04: reg[a] = reg[a] - reg[b]; break;
+        case 0x05: reg[a] = b; break;
+        case 0x0A: running = 0; break;
+    }
+}
