2e8c73a6325a6c9dfa5b553c505292824dba8fc8
Bruno Augusto <bruno.filho1000@gmail.com>
2026-03-19 10:10:20 -0300

    Add initial implementation of memory and CPU simulation

---
+++ pessoa1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t mem[256] = {0};
+uint8_t reg[4] = {0};
+uint8_t pc = 0, zf = 0, running = 1;
+int ciclo = 0;
+
+void fetch(uint8_t *op, uint8_t *a, uint8_t *b) {
+    *op = mem[pc]; *a = mem[pc+1]; *b = mem[pc+2];
+    pc += 3;
+}
+
+int main() {
+    carregar_memoria();
+    while (running && pc < 256) {
+        uint8_t op, a, b;
+        ciclo++;
+        fetch(&op, &a, &b);
+        decode_execute(op, a, b);
+        trace(op, a, b);
+    }
+    printf("\nResultado armazenado no endereco de memoria 0x20 (decimal 32): %d\n", mem[0x20]);
+    return 0;
+}
